789b292a5e74b54b539e55fe13186ae4df84cca8
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2024-10-09 22:44:16 +0500

    js 1st assignment complete

---
+++ app.js
@@ -0,0 +1,45 @@
+alert("Error! Please Enter Valid A Name")
+
+        var book = ("A Smarter\nway To learn javascript")
+        alert("I am trying to learn from the book " + book)
+
+        var email = ("muhammadhasnain12446@gmail.com")
+        alert("My Email Addres is " + email)
+
+        var js = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+        alert(js)
+
+        var studentname = "hasnain";
+        var studentage = 20
+        var classname = "2nd"
+        var subject = "english";
+
+        alert("Name" + " " + studentname)
+        alert("student age" + " " + studentage)
+        alert("class" + " " + classname)
+        alert("subject" + " " + subject)
+
+        alert ("Welcome to JS Land...\n Happy Coding!")
+
+
+        var age = (15)
+        alert ("I am "+ age +" Year Old"  ) 
+
+        var visit = (14)
+        alert ("You have visited this site " + visit + " times")
+
+        document.write("Yah I can write HTML content through java script <br> <br>")
+
+
+        var birthyear = (1990)
+        document.write("My birth year is " + birthyear + "<br> <br>")
+
+
+
+
+
+        var name = (" john Doe")
+        var product = (" T-Shirt ")
+        var quantity = (5)
+        document.write(name + " ordered " + quantity + product + "on XYZ Clothing Store <br>")
+ 
